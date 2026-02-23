11c13f670c0c5f2a8a5cbf042cdf69525fff4237
chamara <chamaradharmarathna6@gmail.com>
2026-02-23 09:30:57 +0530

    add User entity model

---
+++ src/main/java/com/oceanview/model/User.java
@@ -0,0 +1,120 @@
+package com.oceanview.model;
+
+import java.io.Serializable;
+
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String userId;
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+    private String role;
+    private boolean active;
+    private long createdDate;
+    private String createdBy;
+
+
+    public User() {
+    }
+
+    public User(String userId, String username, String password, String fullName,
+                String email, String role, boolean active) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.email = email;
+        this.role = role;
+        this.active = active;
+        this.createdDate = System.currentTimeMillis();
+    }
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public long getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(long createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", username='" + username + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                ", active=" + active +
+                '}';
+    }
+}
+
